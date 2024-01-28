8fb2e05dedcdf3163c4f7ccf7f8eb180fb7e08d4
Kang-bh <kabyho3513@gmail.com>
2024-01-29 02:53:04 +0900

    add blog screen with mvc

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (title, content) VALUES ('title1', 'content 1')
-INSERT INTO article (title, content) VALUES ('title2', 'content 2')
-INSERT INTO article (title, content) VALUES ('title3', 'content 3')
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title1', 'content 1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title2', 'content 2', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title3', 'content 3', NOW(), NOW())
