b8c7e3907cef608bd7005d9a7f62a29a5350c115
Kang-bh <kabyho3513@gmail.com>
2024-08-02 02:14:36 +0900

    [Spring Security] Add: check session info

--- SpringSecurity/src/main/java/org/study/springsecurity/controller/MainController.java
+++ SpringSecurity/src/main/java/org/study/springsecurity/controller/MainController.java
@@ -1,14 +1,35 @@
 package org.study.springsecurity.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) { // model interface 생성
+        // todo : service 단 만들어서 작성하기
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+
+        // todo : 특정 role 값 통해 내부 검증로직에 사용
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
 
         return "main";
     }
