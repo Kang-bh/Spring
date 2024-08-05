3c0a0ea544e6a03f3eb7789c95b2ede882014fd2
Kang-bh <kabyho3513@gmail.com>
2024-08-05 14:29:27 +0900

    [SpringSecurityOAuthJwt] setting base security config

---
+++ SpringSecurityOAuthJWT/src/main/java/org/study/springsecurityoauthjwt/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package org.study.springsecurityoauthjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth) -> auth.disable());
+
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // Oauth2
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        // Session : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+
+    }
+}
