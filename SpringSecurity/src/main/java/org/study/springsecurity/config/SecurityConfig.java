603279ae78918d95592da7c991de974b63c2b62b
Kang-bh <kabyho3513@gmail.com>
2024-08-05 10:09:17 +0900

    [Spring Security] Add: InMemoryUserService

--- SpringSecurity/src/main/java/org/study/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/org/study/springsecurity/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // Spring Boot에게 Configuration으로 등록 필요
@@ -62,5 +66,24 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    // InMemoryUser
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("user2")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 
 }
