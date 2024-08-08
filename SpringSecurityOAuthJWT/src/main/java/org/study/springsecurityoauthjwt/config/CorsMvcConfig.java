adad021dd945a23b4dd6a3a2b93fed7bc779869b
Kang-bh <kabyho3513@gmail.com>
2024-08-08 09:44:20 +0900

    [SpringSecurityOauthJWT] Add: cors

---
+++ SpringSecurityOAuthJWT/src/main/java/org/study/springsecurityoauthjwt/config/CorsMvcConfig.java
@@ -0,0 +1,19 @@
+package org.study.springsecurityoauthjwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+// Controller 단에서 전송하는 정보를 받기 위해
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
+
+    }
+}
--- SpringSecurityOAuthJWT/src/main/java/org/study/springsecurityoauthjwt/config/SecurityConfig.java
+++ SpringSecurityOAuthJWT/src/main/java/org/study/springsecurityoauthjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.study.springsecurityoauthjwt.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,10 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.study.springsecurityoauthjwt.Service.CustomOAuth2UserService;
 import org.study.springsecurityoauthjwt.jwt.JWTFilter;
 import org.study.springsecurityoauthjwt.jwt.JWTUtil;
 import org.study.springsecurityoauthjwt.oauth2.CustomSuccessHandler;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +38,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // 프론트 서버 주소
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
 
         http
                 .csrf((auth) -> auth.disable());
